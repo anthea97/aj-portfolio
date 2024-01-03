1c2f6482b08f4f53c1dae843b91e6525ff37c546
Anthea Abreo <anthea.abreo@gmail.com>
2024-01-03 10:58:13 -0500

    Add header component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
