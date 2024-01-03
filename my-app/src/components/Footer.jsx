907dc7246d6f975d54d62cdd31ab62c09441ad1c
Anthea Abreo <anthea.abreo@gmail.com>
2024-01-03 11:46:04 -0500

    Header updates

---
+++ my-app/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer class="text-center text-lg-start bg-body-tertiary text-muted">
+      <div class="text-center p-4">© TheaBreo 2024</div>
+    </footer>
+  );
+};
+
+export default Footer;
--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -1,9 +1,9 @@
 const Header = () => {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container-fluid">
         <a class="navbar-brand" href="#">
-          Navbar
+          AJ{" "}
         </a>
         <button
           class="navbar-toggler"
@@ -25,63 +25,25 @@ const Header = () => {
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">
-                Link
+                Projects
               </a>
-            </li>
-            <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Dropdown
-              </a>
-              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Action
-                  </a>
-                </li>
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Another action
-                  </a>
-                </li>
-                <li>
-                  <hr class="dropdown-divider"></hr>
-                </li>
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </li>
-              </ul>
             </li>
             <li class="nav-item">
-              <a
-                class="nav-link disabled"
-                href="#"
-                tabindex="-1"
-                aria-disabled="true"
-              >
-                Disabled
+              <a class="nav-link" href="#">
+                Experience
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Resume
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Contact
               </a>
             </li>
           </ul>
-          <form class="d-flex">
-            <input
-              class="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            ></input>
-            <button class="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
         </div>
       </div>
     </nav>
